6173c0b3194014fd23eb756c4556fce6b3614118
jayverts <j.verdolino@gmail.com>
2017-08-24 22:53:31 -0600

    styled landing page

--- anima/src/app/app.module.ts
+++ anima/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
 import { NewJournalComponent } from './new-journal/new-journal.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewUserComponent,
     NewEntryComponent,
-    NewJournalComponent
+    NewJournalComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'anima'})
