0c3cfdad8841b251852ff4c28e8d12de16d1b029
cg923 <coreygselover@gmail.com>
2017-08-26 09:31:28 -0600

    User can now cycle through journals with buttons

--- anima/src/app/all-journals/all-journals.component.ts
+++ anima/src/app/all-journals/all-journals.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-all-journals',
@@ -7,25 +7,50 @@ import { Component, AfterViewInit, ElementRef } from '@angular/core';
 })
 export class AllJournalsComponent implements AfterViewInit {
 
+  selected = 0;
+
   journals = [
       {
         id: 1,
         name: 'Daily Journal',
-        color: 'blue'
+        color: 'blue',
+        selected: true
       },
       {
         id: 2,
         name: 'Creative Writing',
-        color: 'red'
+        color: 'red',
+        selected: false
+      },
+      {
+        id: 3,
+        name: 'Questions',
+        color: 'orange',
+        selected: false
       }
   ];
 
-  selected;
+  updateSelected() {
+    // Unselect other journals
+    this.journals.forEach(element => element.selected = false);
+    // Select new journal
+    this.journals[this.selected].selected = true;
+  }
 
-  constructor(private elementRef: ElementRef) { }
+  cycleLeft() {
+    if(this.selected > 0)
+      this.selected--;
+    this.updateSelected();
+  }
+
+  cycleRight() {
+    if(this.selected < this.journals.length - 1)
+      this.selected++;
+    this.updateSelected();
+  }
+
+  constructor() { }
 
   ngAfterViewInit() {
-
-    this.selected = this.journals[1].id;
   }
 }
