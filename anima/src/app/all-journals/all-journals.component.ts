e536c796e527ff9b1647426fc0355e276399230d
cg923 <coreygselover@gmail.com>
2017-08-26 11:23:57 -0600

    Reworks carousel.  No animations but works nicely

--- anima/src/app/all-journals/all-journals.component.ts
+++ anima/src/app/all-journals/all-journals.component.ts
@@ -9,53 +9,69 @@ export class AllJournalsComponent implements AfterViewInit {
 
   selected = 0;
 
+  journals_left = [];
   journals = [
-      {
-        id: 1,
-        name: 'Daily Journal',
-        color: 'blue',
-        selected: true,
-        moving: false
-      },
-      {
-        id: 2,
-        name: 'Creative Writing',
-        color: 'red',
-        selected: false,
-        moving: false
-      },
-      {
-        id: 3,
-        name: 'Questions',
-        color: 'orange',
-        selected: false,
-        moving: false
-      }
+    {
+      id: 1,
+      name: 'Daily Journal',
+      color: 'blue',
+      selected: true,
+      moving_left: false,
+      moving_right: false
+    }
   ];
+  journals_right = [
+    {
+      id: 2,
+      name: 'Creative Writing',
+      color: 'red',
+      selected: false,
+      moving_left: false,
+      moving_right: false
+    },
+    {
+      id: 3,
+      name: 'Questions',
+      color: 'orange',
+      selected: false,
+      moving_left: false,
+      moving_right: false
+    }
+  ]
 
   updateSelected() {
     // Unselect other journals
-    this.journals.forEach(element => {
-      element.selected = false;
-      element.moving = true;
-      setTimeout(() => { element.moving = false }, 500);
-    });
+    this.journals_left.forEach(element => element.selected = false );
+    this.journals_right.forEach(element => element.selected = false);
     // Select new journal
-    this.journals[this.selected].selected = true;
+    this.journals[0].selected = true;
   }
 
   cycleLeft() {
-    if(this.selected > 0) {
-      this.selected--;
+    if(this.journals_right.length > 0) {
+      this.journals_left.push(this.journals.pop());
+      this.journals.push(this.journals_right.pop());
       this.updateSelected();
     }
   }
 
   cycleRight() {
-    if(this.selected < this.journals.length - 1) {
-      this.selected++;
+    if(this.journals_left.length > 0) {
+      this.journals_right.push(this.journals.pop());
+      this.journals.push(this.journals_left.pop());
       this.updateSelected();
     }
+  }
+
+  // In the following two functions, we return id !== id
+  // because we want to return TRUE if the journal should
+  // NOT be visible.
+  visibleJournalLeft(id) {
+    return id !== this.journals_left[this.journals_left.length -1].id;
+  }
+
+  visibleJournalRight(id) {
+    return id !== this.journals_right[0].id;
   }
 
   constructor() { }
