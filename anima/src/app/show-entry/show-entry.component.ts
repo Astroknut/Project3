64e21bac86758a1b142d75545f321991067f8af1
cg923 <coreygselover@gmail.com>
2017-08-29 10:51:56 -0600

    Can now edit a journal entry

--- anima/src/app/show-entry/show-entry.component.ts
+++ anima/src/app/show-entry/show-entry.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowEntryComponent implements OnInit {
 
+  editing = false;
+  oldText = "";
+
   entry = {
   	date: "8/26/17",
   	text: "Donald Trump showed up at my house and I punched him right in the kisser."
+  }
+
+  editEntry() {
+  	this.editing = true;
+  	this.oldText = this.entry.text;
+  }
+
+  saveEntry() {
+  	this.editing = false;
+  }
+
+  cancelEdit() {
+  	this.editing = false;
+  	this.entry.text = this.oldText;
   }
 
   constructor() { }
