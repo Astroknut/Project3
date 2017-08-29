eaf5e6dc828eaa57c8ac637c4a1137ba3c8c861d
roxnlopez <roxnlopez@gmail.com>
2017-08-29 12:17:49 -0600

    added routes

--- anima/src/app/app.module.ts
+++ anima/src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
 import { NewJournalComponent } from './new-journal/new-journal.component';
+import { GuardsComponent } from './guards/guards.component';
+import { HelpersComponent } from './helpers/helpers.component';
+import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewUserComponent,
     NewEntryComponent,
-    NewJournalComponent
+    NewJournalComponent,
+    GuardsComponent,
+    HelpersComponent,
+    ModelsComponent,
+    ServicesComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'anima'})
--- anima/src/config/routes.ts
+++ anima/src/config/routes.ts
@@ -1,4 +1,28 @@
 import * as express from 'express';
 var router = express.Router();
 
+//GET home 
+router.route('/all-journals')
+	.get();
+
+//GET a journal
+router.route('/show-journal')
+	.get();
+
+//GET a journal entry
+router.route('/show-entry')
+	.get();
+
+//UPDATE journal entry
+router.route('/update-entry')
+	.post();
+
+//DELETE a journal entry
+router.route('/delete-entry')
+	.remove();
+
+//DELETE a journal
+router.route('/delete-journal')
+	.remove();				
+
 export {router};
