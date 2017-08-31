39f822371e331e14afa5bafbc3ed497712f5bd7e
cg923 <coreygselover@gmail.com>
2017-08-31 09:01:42 -0600

    Making changes for heroku deployment

--- anima/src/models/index.ts
+++ anima/src/models/index.ts
@@ -1,6 +1,6 @@
 import * as Sequelize from 'sequelize';
 
-var sequelize = new Sequelize('postgres://corey@localhost:5432/project3');
+var sequelize = new Sequelize('postgres://glnfavglxpsssx:ff7261a5374620184780aa690996db8a96583033474742236ebb5d193396fbb6@ec2-54-225-113-161.compute-1.amazonaws.com:5432/d76tla24vfd6ns');
 
 var Entry = sequelize.import("./entry");
 var Journal = sequelize.import('./journal');
