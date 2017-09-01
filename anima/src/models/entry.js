42e12c298a91d826c7532ac42407871f092e7de7
cg923 <coreygselover@gmail.com>
2017-09-01 11:19:30 -0600

    Preparing for Heroku pull

--- anima/src/models/entry.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, Sequelize) {
-	var model = sequelize.define("entry", {
-		text: Sequelize.STRING,
-		date: Sequelize.DATE
-	});
-	return model;
-};
--- anima/src/models/journal.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, Sequelize) {
-	var model = sequelize.define("journal", {
-		name: Sequelize.STRING,
-		color: Sequelize.STRING,
-	});
-	return model;
-};
